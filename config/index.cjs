c0475541f51ae97034157e10b47c1225fbdb78a2
xiaocai725 <147219486+xiaocai725@users.noreply.github.com>
2024-08-16 23:39:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ocmSu6FOp1Y9pQBghpNeRavZa6Hw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '-XcltFBSHwsfCXkLZTZlnTBk6zWz6sQSJET_KT-_JBE',
+      useTemplateId: '01YMM0mk4SFLOumJS6XNwMq5K4PWYZKsyBC7w1WGdV4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-19',
       festivals: [
