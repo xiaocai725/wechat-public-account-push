13aaa4043201a1441a64cb83cf41ef565982d8da
xiaocai725 <147219486+xiaocai725@users.noreply.github.com>
2024-08-16 23:25:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '小才',
+      name: 'xiaoyang',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ocmSu6LHmn_if3NCUr35Olq3JIKQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Z-K8sF2P3VNwBgPb8m5WTqmplJeVkplGKM0l1Omm5wE',
+      useTemplateId: 'FR8MA_AG_JJjzGUpdzohab3teQiAsNz7Svj-_L9-ku4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-25',
       festivals: [
